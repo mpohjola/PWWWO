4541809afdee233d2404daf9124b268ee5ddbfa7
Mira Pohjola <mira.pohjola89@gmail.com>
2018-10-12 03:34:03 -0700

    Simple node server example with fibonacci number calculating with views and routes.

---
+++ 2018_09_28/fibonacci/routes/fibonacci.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var math = require("../math");
+
+router.get('/', function(req, res, next) {
+    if (req.query.fibonum) {
+        //calculate fibonacci numbers
+        res.render('fibonacci', {
+            title:"Calculate Fibonacci numbers",
+            fibonum: req.query.fibonum,
+            fiboval: math.fibonacci(req.query.fibonum)
+        });
+    } else {
+        res.render('fibonacci',{
+            title: "Calculate Fibonacci numbers",
+            fiboval: undefined
+        });
+    }
+});
+
+module.exports = router;
