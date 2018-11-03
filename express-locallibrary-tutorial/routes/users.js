181a1b60f39088ba6dfd8d0d4841bceb6fcf3c1b
Mira Pohjola <Mira.Pohjola89@gmail.com>
2018-11-03 20:05:15 +0200

    Challenge for the skeleton website part of the tutorial.

--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.get('/cool', (req, res, next) => {
+  res.send(`You're so cool`);
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
