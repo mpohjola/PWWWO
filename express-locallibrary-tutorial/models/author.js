8e50a2b6c1246ce5c9114be79a56ecb948223e08
Mira Pohjola <Mira.Pohjola89@gmail.com>
2018-11-05 19:44:46 +0200

    Added virtual properties for fullname, lifespan and URL. Also added required nationality field in the Schema definition.

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -6,7 +6,28 @@ var AuthorSchema = new Schema(
     {
         first_name: {type:String, required: true, max:100},
         family_name: {type: String, required:true, max:100},
+        nationality: {type: String, max:100},
         date_of_birth: {type: Date},
         date_of_death: {type: Date}
     }
 );
+
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+});
+
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+module.exports = mongoose.model('Author', AuthorSchema);
