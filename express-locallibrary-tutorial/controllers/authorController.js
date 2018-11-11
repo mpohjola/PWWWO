c9368c5a7aa32a3c805fc46fb3f703093c8f80d7
Mira Pohjola <Mira.Pohjola89@gmail.com>
2018-11-11 14:20:03 +0200

    Added author detail view and made related changes to authorController.
    Additionally added a virtual field to Author model to format date of death to something more coherent and modified author details and listing to use this formatted version.

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -1,4 +1,7 @@
 var Author = require('../models/author');
+var Book = require('../models/book');
+
+var async = require('async');
 
 exports.author_list = (req, res, next) => {
     Author.find()
@@ -10,8 +13,26 @@ exports.author_list = (req, res, next) => {
     });
 };
 
-exports.author_detail = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+exports.author_detail = (req, res, next) => {
+    async.parallel({
+        author: (callback) => {
+            Author.findById(req.params.id)
+              .exec(callback)
+        },
+        authors_books: (callback) => {
+          Book.find({ 'author': req.params.id },'title summary')
+          .exec(callback)
+        },
+    }, (err, results) => {
+        if (err) { return next(err); } // Error in API usage.
+        if (results.author == null) { // No results.
+            var err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render.
+        res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books } );
+    });
 };
 
 exports.author_create_get = (req, res) => {
--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -36,6 +37,12 @@ AuthorSchema
 .virtual('date_of_birth_formatted')
 .get(function () {
   return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+});
+
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+  return this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
 });
 
 module.exports = mongoose.model('Author', AuthorSchema);
