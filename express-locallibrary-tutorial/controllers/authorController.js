c6df151e7ca7a442ea68bde334c0194c98b39698
Mira Pohjola <Mira.Pohjola89@gmail.com>
2018-11-17 14:04:23 +0200

    Made needed changes to authorController to handle new author creation. Also included the nationality field required in the exercise. Nationality is an optional parameter that is expected to be alpha and validated as such.

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -1,3 +1,6 @@
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 var Author = require('../models/author');
 var Book = require('../models/book');
 
@@ -5,23 +8,23 @@ var async = require('async');
 
 exports.author_list = (req, res, next) => {
     Author.find()
-    .sort([['family_name', 'ascending']]) //sorts authors by surname
-    .exec((err, list_authors) => {
-      if (err) { return next(err); }
-      //Successful, so render
-      res.render('author_list', { title: 'Author List', author_list: list_authors });
-    });
+        .sort([['family_name', 'ascending']]) //sorts authors by surname
+        .exec((err, list_authors) => {
+            if (err) { return next(err); }
+            //Successful, so render
+            res.render('author_list', { title: 'Author List', author_list: list_authors });
+        });
 };
 
 exports.author_detail = (req, res, next) => {
     async.parallel({
         author: (callback) => {
             Author.findById(req.params.id)
-              .exec(callback)
+                .exec(callback)
         },
         authors_books: (callback) => {
-          Book.find({ 'author': req.params.id },'title summary')
-          .exec(callback)
+            Book.find({ 'author': req.params.id }, 'title summary')
+                .exec(callback)
         },
     }, (err, results) => {
         if (err) { return next(err); } // Error in API usage.
@@ -31,17 +34,64 @@ exports.author_detail = (req, res, next) => {
             return next(err);
         }
         // Successful, so render.
-        res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books } );
+        res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books });
     });
 };
 
-exports.author_create_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author create GET');
+exports.author_create_get = (req, res, next) => {
+    res.render('author_form', { title: 'Create Author' });
 };
 
-exports.author_create_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+
+    // Validate fields.
+    body('first_name').isLength({ min: 1 }).trim().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').isLength({ min: 1 }).trim().withMessage('Family name must be specified.')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601(),
+    body('nationality').optional({ checkFalsy: true }).isAlpha().withMessage('Nationality has non-alpha characters'),
+
+    // Sanitize fields.
+    sanitizeBody('first_name').trim().escape(),
+    sanitizeBody('family_name').trim().escape(),
+    sanitizeBody('date_of_birth').toDate(),
+    sanitizeBody('date_of_death').toDate(),
+    sanitizeBody('nationality').trim().escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('author_form', { title: 'Create Author', author: req.body, errors: errors.array() });
+            return;
+        }
+        else {
+            // Data from form is valid.
+
+            // Create an Author object with escaped and trimmed data.
+            var author = new Author(
+                {
+                    first_name: req.body.first_name,
+                    family_name: req.body.family_name,
+                    nationality: req.body.nationality,
+                    date_of_birth: req.body.date_of_birth,
+                    date_of_death: req.body.date_of_death
+                    
+                });
+            author.save((err) => {
+                if (err) { return next(err); }
+                // Successful - redirect to new author record.
+                res.redirect(author.url);
+            });
+        }
+    }
+];
 
 exports.author_delete_get = (req, res) => {
     res.send('NOT IMPLEMENTED: Author delete GET');
