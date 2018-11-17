b6c8305d91730e992771eb7c155fc7030a85b205
Mira Pohjola <Mira.Pohjola89@gmail.com>
2018-11-17 14:02:30 +0200

    Added view for adding genres as well as made related changes to genre controller.

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -1,3 +1,6 @@
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 var Genre = require('../models/genre');
 var Book = require('../models/book');
 var async = require('async');
@@ -5,12 +8,12 @@ var async = require('async');
 // Display list of all Genre.
 exports.genre_list = (req, res, next) => {
     Genre.find()
-    .sort([['name', 'ascending']]) //sorts genres alphabetically
-    .exec((err, list_genres) => {
-      if (err) { return next(err); }
-      //Successful, so render
-      res.render('genre_list', { title: 'Genre List', genre_list: list_genres });
-    });
+        .sort([['name', 'ascending']]) //sorts genres alphabetically
+        .exec((err, list_genres) => {
+            if (err) { return next(err); }
+            //Successful, so render
+            res.render('genre_list', { title: 'Genre List', genre_list: list_genres });
+        });
 };
 
 // Display detail page for a specific Genre.
@@ -18,12 +21,12 @@ exports.genre_detail = (req, res, next) => {
     async.parallel({
         genre: (callback) => {
             Genre.findById(req.params.id)
-              .exec(callback);
+                .exec(callback);
         },
 
         genre_books: (callback) => {
-          Book.find({ 'genre': req.params.id })
-          .exec(callback);
+            Book.find({ 'genre': req.params.id })
+                .exec(callback);
         },
 
     }, (err, results) => {
@@ -34,19 +37,66 @@ exports.genre_detail = (req, res, next) => {
             return next(err);
         }
         // Successful, so render
-        res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books } );
+        res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books });
     });
 };
 
 // Display Genre create form on GET.
-exports.genre_create_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+exports.genre_create_get = (req, res, next) => {
+    res.render('genre_form', { title: 'Create Genre' });
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: Genre create POST');
-};
+exports.genre_create_post = [
+
+    // Validate that the name field is not empty.
+    body('name', 'Genre name required').isLength({ min: 1 }).trim(),
+
+    // Sanitize (trim and escape) the name field.
+    sanitizeBody('name').trim().escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a genre object with escaped and trimmed data.
+        var genre = new Genre(
+            { name: req.body.name }
+        );
+
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render the form again with sanitized values/error messages.
+            res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array() });
+            return;
+        }
+        else {
+            // Data from form is valid.
+            // Check if Genre with same name already exists.
+            Genre.findOne({ 'name': req.body.name })
+                .exec((err, found_genre) => {
+                    if (err) { return next(err); }
+
+                    if (found_genre) {
+                        // Genre exists, redirect to its detail page.
+                        res.redirect(found_genre.url);
+                    }
+                    else {
+
+                        genre.save((err) => {
+                            if (err) { return next(err); }
+                            // Genre saved. Redirect to genre detail page.
+                            res.redirect(genre.url);
+                        });
+
+                    }
+
+                });
+        }
+    }
+];
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = (req, res) => {
